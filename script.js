b0872c67679babad7193a41275e1ad1f923d1717
0904teacher <0729teachers@gmail.com>
2023-09-12 09:32:04 +0900

    main

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function fetchValue() {
-  fetch('https://script.google.com/macros/s/AKfycbw6-E_lx-a-_fXebPPAe3-RTFaTxQYf-xqyZdM8gW1Q0M8N8I3STVyKPxeTIkp-eese5w/exec') // 위에서 받은 웹 앱의 URL을 입력하세요
+  fetch('https://script.google.com/macros/s/AKfycby2NkzHnRGZSba4xKLofHUjWbg6-AmR5vqaLoHtNrZNqram9vJ8gOMuIHo6HkOd9fXtKA/exec') // 위에서 받은 웹 앱의 URL을 입력하세요
   .then(response => response.text())
   .then(data => {
     document.getElementById('value').innerText = data;
